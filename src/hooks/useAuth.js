5a1c88a1ca17978de40c3cf437ccefd8793d0b8f
Tu Nombre <tu@correo.com>
2026-03-19 18:26:31 -0300

    feat: crear feature/primero con README y hook useAuth

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,1 +1,1 @@
-export default function useAuth() {\r\n  // Fix experimental: retornar objeto de usuario simulado\r\n  return { user: { id: 1, name: 'Demo' } };\r\n}\r\n
+export default function useAuth() {\r\n  // Versión final: retornar objeto de usuario simulado\r\n  return { user: { id: 1, name: 'Demo' } };\r\n}\r\n
